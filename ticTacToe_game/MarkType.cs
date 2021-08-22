94a256da2017bbe8ff11427760772bcc89a69d88
Nabeel Asif <nabeel.asif362@gmail.com>
2021-08-22 23:23:04 +0500

    add files

---
+++ ticTacToe_game/MarkType.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ticTacToe_game
+{
+    /// <summary>
+    /// The type of a value a cell in the game is currently at
+    /// </summary>
+    public enum MarkType
+    {
+        /// <summary>
+        /// The cell hasn't  been clicked yet
+        /// </summary>
+        Free,
+        /// <summary>
+        /// The cell is a O
+        /// </summary>
+        Nought,
+        /// <summary>
+        /// The Cell is an X
+        /// </summary>
+        Cross
+    }
+}
